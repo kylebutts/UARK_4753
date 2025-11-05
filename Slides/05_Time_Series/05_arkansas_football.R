37d5198e8d1773b93cdab11bf12d1afcb0a457e2
Kyle Butts <buttskyle96@gmail.com>
2025-11-05 14:21:28 -0600

    Merge time-series slides and put regression before

---
+++ Slides/05_Time_Series/05_arkansas_football.R
@@ -0,0 +1,92 @@
+# %%
+library(tidyverse)
+library(here)
+
+football <- tibble(
+  date = c(
+    "11-11-2023",
+    "11-04-2023",
+    "09-23-2023",
+    "09-02-2023",
+    "10-07-2023",
+    "09-16-2023",
+    "09-09-2023",
+    "10-21-2023",
+    "11-24-2023",
+    "10-14-2023",
+    "11-18-2023",
+    "09-30-2023"
+  ),
+  school = rep("Arkansas", 12L),
+  opponent = c(
+    "Auburn",
+    "Florida",
+    "(12) LSU",
+    "Western Carolina",
+    "(16) Ole Miss",
+    "BYU",
+    "Kent State",
+    "Mississippi State",
+    "(10) Missouri",
+    "(11) Alabama",
+    "Florida International",
+    "Texas A&M"
+  ),
+  result = c("L", "W", "L", "W", "L", "L", "W", "L", "L", "L", "W", "L"),
+  pts = c(10, 39, 31, 56, 20, 31, 28, 3, 14, 21, 44, 22),
+  pts_opponent = c(48, 36, 34, 13, 27, 38, 6, 7, 48, 24, 20, 34)
+)
+football$date <- football$date |>
+  parse_date_time(orders = "%m-%d-%Y") |>
+  as_date()
+
+
+# %%
+(p_football <- ggplot() +
+  geom_line(
+    aes(x = date, y = pts_opponent, color = "Opponent"),
+    data = football,
+    linewidth = 1.25
+  ) +
+  geom_point(
+    aes(x = date, y = pts_opponent, color = "Opponent"),
+    data = football
+  ) +
+  geom_line(
+    aes(x = date, y = pts, color = "Arkansas"),
+    data = football,
+    linewidth = 1.25
+  ) +
+  geom_point(
+    aes(x = date, y = pts, color = "Arkansas"),
+    data = football
+  ) +
+  scale_x_date() +
+  scale_color_manual(
+    values = c(
+      "Arkansas" = "#9D2235",
+      "Opponent" = kfbmisc::tailwind_color("zinc-500")
+    )
+  ) +
+  labs(
+    x = NULL,
+    y = "Points Scored",
+    color = NULL
+  ) +
+  kfbmisc::theme_kyle(base_size = 14) +
+  theme(
+    legend.position = "top",
+    legend.margin = margin(0, 0, 5, 0),
+    legend.justification = c(0, 1),
+    legend.location = "plot"
+  ))
+
+# %%
+kfbmisc::tikzsave(
+  here("Slides/05_Time_Series/figures/arkansas_2023_football.pdf"),
+  plot = p_football,
+  width = 8,
+  height = 4.5
+)
+
+# %%
---
+++ Slides/05_Time_Series/05_linear_time_trend_gdp.R
@@ -0,0 +1,80 @@
+# https://people.duke.edu/~rnau/411home.htm
+# %%
+library(tidyverse)
+library(here)
+library(fixest)
+
+# https://people.duke.edu/~rnau/Slides_on_forecasting_with_inflation_seasonal_adjustment_and_Winters_model--Robert%20Nau.pdf
+df <- here("Slides/05_Time_Series/data/GDP.csv") |>
+  read_csv(show_col_types = FALSE) |>
+  select(date = DATE, gdp = GDP) |>
+  filter(year(date) >= 2000)
+
+# %%
+(plot_raw <- ggplot(df) +
+  geom_line(
+    aes(x = date, y = gdp),
+    color = kfbmisc::tailwind_color("zinc-600"),
+    linewidth = 1
+  ) +
+  labs(x = NULL, y = "U.S. GDP (\\$ Billions)") +
+  kfbmisc::theme_kyle(base_size = 12))
+
+est_linear_trend <- feols(
+  gdp ~ date,
+  data = df,
+  vcov = "hc1"
+)
+df$gdp_hat <- predict(est_linear_trend)
+
+(plot_linear_trend <-
+  plot_raw +
+  geom_line(
+    aes(x = date, y = gdp_hat),
+    data = df,
+    color = kfbmisc::kyle_color("blue"),
+    linewidth = 1
+  ))
+
+# %%
+predictions <- predict(est_linear_trend, se.fit = TRUE)
+head(predictions)
+
+# The next 7 days from our sample
+prediction_df <- data.frame(
+  date = ymd("2021-06-30") + 1:5
+)
+predictions <- predict(est_linear_trend, newdata = prediction_df, se.fit = TRUE)
+predictions$ci_lower <- predictions$fit - 1.96 * predictions$se.fit
+predictions$ci_upper <- predictions$fit + 1.96 * predictions$se.fit
+
+# %%
+library(tsibble)
+df$month = yearmonth(df$date)
+df$quarter = yearquarter(df$date)
+# Does not change the predictions, just the interpretation of coefficients
+# This is not exactly true since months have varying number of days
+feols(
+  gdp ~ month,
+  data = df,
+  vcov = "hc1"
+)
+feols(
+  gdp ~ quarter,
+  data = df,
+  vcov = "hc1"
+)
+
+# %%
+kfbmisc::tikzsave(
+  here::here("Slides/05_Time_Series/figures/gdp_raw.pdf"),
+  plot_raw,
+  width = 8,
+  height = 3.8
+)
+kfbmisc::tikzsave(
+  here::here("Slides/05_Time_Series/figures/gdp_linear_trend.pdf"),
+  plot_linear_trend,
+  width = 8,
+  height = 3.8
+)
