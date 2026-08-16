15779441322bb9055d0764cde68952a888394224
Kyle Butts <buttskyle96@gmail.com>
2026-08-16 11:22:17 -0500

    Clean up labs from F2025

--- R_Labs/Lab_09/data/parse_bitcoin_daily.R
+++ R_Labs/Lab_09/data/parse_bitcoin_daily.R
@@ -1,11 +1,11 @@
-# %% 
+# %%
 library(tidyverse)
 library(fixest)
 library(rvest)
 library(here)
 library(fs)
 
-rows <- here("R_Labs/Lab_9/data/bitcoin_daily.html") |>
+rows <- here("R_Labs/Lab_09/data/bitcoin_daily.html") |>
   read_html() |>
   html_element("tbody") |>
   html_elements("tr")
@@ -55,29 +55,34 @@ parse_row <- function(row) {
     parse_number()
 
   tibble(
-    date, price_open, price_high, price_low, price_close, price_adj_close, volume
+    date,
+    price_open,
+    price_high,
+    price_low,
+    price_close,
+    price_adj_close,
+    volume
   )
 }
 
-# %% 
+# %%
 # https://finance.yahoo.com/quote/BTC-USD/history/?period1=1410912000&period2=1732503478
 bitcoin_prices <- map(rows, parse_row) |>
   list_rbind()
 
-fs::dir_create(here("R_Labs/Lab_9/data/"))
+fs::dir_create(here("R_Labs/Lab_09/data/"))
 write_csv(
-  bitcoin_prices, 
-  file = here("R_Labs/Lab_9/data/bitcoin_daily.csv")
+  bitcoin_prices,
+  file = here("R_Labs/Lab_09/data/bitcoin_daily.csv")
 )
 
-# %% 
-bitcoin_prices <- read_csv(here("R_Labs/Lab_9/data/bitcoin_daily.csv"))
+# %%
+bitcoin_prices <- read_csv(here("R_Labs/Lab_09/data/bitcoin_daily.csv"))
 bitcoin_prices <- bitcoin_prices |>
   filter(date >= ymd("2022-01-01"))
 
 ggplot() +
   geom_line(
-    aes(x = date, y = price_close), 
+    aes(x = date, y = price_close),
     data = bitcoin_prices
   )
-
